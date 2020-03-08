0f88c87179da9ed93dd2345a5209387f9cb0716d
Ian <ian@seenit.io>
2020-03-08 15:12:24 +0000

    Remove test hardcode

--- action.js
+++ action.js
@@ -6,8 +6,7 @@ const issueIdRegEx = /([a-zA-Z0-9]+-[0-9]+)/g
 const eventTemplates = {
   branch: '{{event.ref}}',
   commits: "{{event.commits.map(c=>c.message).join(' ')}}",
-  //pr: '{{event.pull_request.head.ref}}',
-  pr: 'SEE-289-test',
+  pr: '{{event.pull_request.head.ref}}',
 }
 
 module.exports = class {
